4d01296d928858dc74a449774ba863a98e19f487
Alexander Adushkin <a.adushkin@inbox.ru>
2020-04-29 23:04:47 +0300

    add new test on login link

--- section4/pages/main_page.py
+++ section4/pages/main_page.py
@@ -8,5 +8,12 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def go_to_login_page_new(self):
+        login_link = self.browser.find_element(By.ID, "registration_link")
+        login_link.click
+
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
+    def should_be_login_link_new(self):
+        assert self.is_element_present(By.ID, "registration_link"), "Login link is not presented"
